270bfcaa260e78f7f19f246bcedce43d07384a2d
Ayush Vaish <72246792+ayushvaish2511@users.noreply.github.com>
2022-01-04 15:51:31 +0530

    Time: 48 ms (11.41%), Space: 51.8 MB (5.99%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,34 +1,35 @@
 class Solution {
     public int firstUniqChar(String s) 
     {
-        // HashMap<Character, Integer> map = new HashMap<>();
-        // for(int i = 0; i<s.length(); i++)
-        // {
-        //     if(map.containsKey(s.charAt(i)))
-        //     {
-        //         map.put(s.charAt(i), map.get(s.charAt(i))+1);
-        //     }
-        //     else 
-        //     {
-        //         map.put(s.charAt(i), 1);
-        //     }
-        // }
-        // for(int i = 0; i<s.length(); i++)
-        // {
-        //     if(map.get(s.charAt(i)) == 1)
-        //         return i;
-        // }
-        // return -1;
-        int ch[] = new int[26];
-        for(int i = 0; i<s.length(); i++)
+        int n = s.length();
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i<n; i++)
         {
-            ch[s.charAt(i) - 'a']++;
+            if(map.containsKey(s.charAt(i)))
+            {
+                map.put(s.charAt(i), map.get(s.charAt(i))+1);
+            }
+            else 
+            {
+                map.put(s.charAt(i), 1);
+            }
         }
-        for(int i = 0; i<s.length(); i++)
+        for(int i = 0; i<n; i++)
         {
-            if(ch[s.charAt(i)-'a'] == 1)
+            if(map.get(s.charAt(i)) == 1)
                 return i;
         }
         return -1;
+        // int ch[] = new int[26];
+        // for(int i = 0; i<s.length(); i++)
+        // {
+        //     ch[s.charAt(i) - 'a']++;
+        // }
+        // for(int i = 0; i<s.length(); i++)
+        // {
+        //     if(ch[s.charAt(i)-'a'] == 1)
+        //         return i;
+        // }
+        // return -1;
     }
 }
