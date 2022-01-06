7f95ad823dca0aba6b31ea5e7d1496a079df01ab
Ayush Vaish <72246792+ayushvaish2511@users.noreply.github.com>
2022-01-06 19:58:24 +0530

    Time: 7 ms (56.79%), Space: 40.3 MB (21.61%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
@@ -1,14 +1,40 @@
 class Solution {
     public int numOfPairs(String[] nums, String target) {
+        HashMap<String, Integer> freq = new HashMap<>();
         int c = 0;
-        for(int i = 0; i<nums.length; i++)
+        for(String str : nums)
         {
-            for(int j = 0; j<nums.length; j++)
+            if(target.startsWith(str))
             {
-                if((nums[i] + nums[j]).equals(target) && i!=j)
-                    c++;
+                String sub = target.substring(str.length());
+                if(freq.containsKey(sub))
+                {
+                    c += freq.get(sub);
+                }
             }
+            if(target.endsWith(str))
+            {
+                String sub = target.substring(0, target.length()-str.length());
+                if(freq.containsKey(sub))
+                {
+                    c += freq.get(sub);
+                }
+            }
+            if(freq.containsKey(str))
+                freq.put(str, freq.get(str) + 1);
+            else 
+                freq.put(str, 1);
         }
         return c;
+        // int c = 0;
+        // for(int i = 0; i<nums.length; i++)
+        // {
+        //     for(int j = 0; j<nums.length; j++)
+        //     {
+        //         if((nums[i] + nums[j]).equals(target) && i!=j)
+        //             c++;
+        //     }
+        // }
+        // return c;
     }
 }
