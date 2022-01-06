660401a18a1213e74f618cb829efcba0b95f66a2
Ayush Vaish <72246792+ayushvaish2511@users.noreply.github.com>
2022-01-06 19:46:42 +0530

    Time: 52 ms (8.39%), Space: 58.1 MB (5.89%) - LeetHub

---
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numOfPairs(String[] nums, String target) {
+        int c = 0;
+        for(int i = 0; i<nums.length; i++)
+        {
+            for(int j = 0; j<nums.length; j++)
+            {
+                if((nums[i] + nums[j]).equals(target) && i!=j)
+                    c++;
+            }
+        }
+        return c;
+    }
+}
