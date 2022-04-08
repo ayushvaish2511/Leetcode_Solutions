620fdafee09753db240e69f2f216df1110240a7f
Ayush Vaish <72246792+ayushvaish2511@users.noreply.github.com>
2022-04-08 23:01:17 +0530

    Time: 11 ms (97.45%), Space: 46.5 MB (86.19%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
@@ -1,20 +1,39 @@
 import java.util.Collections;
 class KthLargest {
-    int k;    
-    ArrayList<Integer> li = new ArrayList<>();
+    // int k;    
+    // ArrayList<Integer> li = new ArrayList<>();
+    // public KthLargest(int k, int[] nums) 
+    // {
+    //     this.k = k;
+    //     for(int i = 0; i<nums.length; i++)
+    //     {
+    //         li.add(nums[i]);
+    //     }
+    // }
+    // public int add(int val) 
+    // {
+    //     li.add(val);
+    //     Collections.sort(li);
+    //     return li.get(li.size()-k);
+    // }
+    int k;
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
     public KthLargest(int k, int[] nums) 
     {
         this.k = k;
         for(int i = 0; i<nums.length; i++)
         {
-            li.add(nums[i]);
+            pq.add(nums[i]);
         }
     }
     public int add(int val) 
     {
-        li.add(val);
-        Collections.sort(li);
-        return li.get(li.size()-k);
+        pq.add(val);
+        while(pq.size()>k)
+        {
+            pq.remove();
+        }
+        return pq.peek();
     }
 }
 
