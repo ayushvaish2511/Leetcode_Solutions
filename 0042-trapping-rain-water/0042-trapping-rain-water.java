1633b45583eb15919c79c4cd1d783790c98c7249
Ayush Vaish <72246792+ayushvaish2511@users.noreply.github.com>
2023-10-14 00:56:47 +0530

    Time: 1 ms (76.13%), Space: 44 MB (81.79%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,29 +1,26 @@
 class Solution {
-    public int maxL(int[] arr, int i)
-    {
-        int ans = arr[i];
-        for( ; i>=0; i--)
-        {
-            ans = ans > arr[i] ? ans : arr[i];
-        }
-        return ans;
-    }
-    public int maxR(int[] arr, int i)
-    {
-        int ans = arr[i];
-        for( ; i<arr.length; i++)
-        {
-            ans = ans > arr[i] ? ans : arr[i];
-        }
-        return ans;
-    }
     public int trap(int[] height) 
     {
+        int n = height.length;
+        int[] prefix = new int[n];
+        int[] postfix = new int[n];
+        int max = 0;
+        for(int i = 0; i<n; i++)
+        {
+            max = max > height[i] ? max : height[i];
+            prefix[i] = max;
+        }
+        max = 0;
+        for(int i = n-1; i>=0; i--)
+        {
+            max = max > height[i] ? max : height[i];
+            postfix[i] = max;
+        }
         int ans = 0;
         for(int i = 1; i<height.length-1; i++)
         {
-            int left = maxL(height, i);
-            int right = maxR(height, i);
+            int left = prefix[i];
+            int right = postfix[i];
             ans += Math.min(left, right) - height[i];
         }
         return ans;
