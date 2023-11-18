0d13256a7c502973951f7f7a040a88b6506a6f3f
Ayush Vaish <72246792+ayushvaish2511@users.noreply.github.com>
2023-11-18 21:35:17 +0530

    Time: 28 ms (95.38%), Space: 55.4 MB (93.58%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -1,41 +1,59 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
         Arrays.sort(nums);
-        int n = nums.length;
-        
+        long sum = 0;
+        int strt = 0;
+        int end;
         int ans = 1;
-        int curr = nums.length-1;
-        while(curr>=0)
+        for(end = 0; end<nums.length; end++)
         {
-            int sum = 0;
-            int c = 1;
-            for(int i = curr-1; i>=0; i--)
+            sum += nums[end];
+            while(sum + k < (long)nums[end] * (end-strt+1))
             {
-                int diff = nums[curr] - nums[i];
-                if(sum + diff <= k)
-                {
-                    sum += diff;
-                    c++;
-                }
-                else 
-                {
-                    break;
-                }
+                sum -= nums[strt];
+                strt++;
             }
-            ans = ans > c ? ans : c;
-            curr--;
-            while(curr >= 0)
-            {
-                if(nums[curr] != nums[curr+1])
-                {
-                    break;
-                }
-                else 
-                {
-                    curr--;
-                }
-            }
+            ans = ans > end-strt+1 ? ans : end-strt+1;
         }
         return ans;
+        
+        
+        //         Arrays.sort(nums);
+//         int n = nums.length;
+        
+//         int ans = 1;
+//         int curr = nums.length-1;
+//         while(curr>=0)
+//         {
+//             int sum = 0;
+//             int c = 1;
+//             for(int i = curr-1; i>=0; i--)
+//             {
+//                 int diff = nums[curr] - nums[i];
+//                 if(sum + diff <= k)
+//                 {
+//                     sum += diff;
+//                     c++;
+//                 }
+//                 else 
+//                 {
+//                     break;
+//                 }
+//             }
+//             ans = ans > c ? ans : c;
+//             curr--;
+//             while(curr >= 0)
+//             {
+//                 if(nums[curr] != nums[curr+1])
+//                 {
+//                     break;
+//                 }
+//                 else 
+//                 {
+//                     curr--;
+//                 }
+//             }
+//         }
+//         return ans;
     }
 }
