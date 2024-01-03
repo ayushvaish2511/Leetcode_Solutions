5f25a507e56eb17495768b9c8a76722524e1ec82
Ayush Vaish <72246792+ayushvaish2511@users.noreply.github.com>
2024-01-03 11:41:11 +0530

    Time: 17 ms (23.02%), Space: 45.6 MB (5.36%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -1,25 +1,21 @@
 class Solution {
-    public int numberOfBeams(String[] bank) 
-    {
-        ArrayList<Integer> c1 = new ArrayList<>();
-        for(int i = 0; i<bank.length; i++)
+    public int numberOfBeams(String[] bank) {
+        int ans = 0;
+        int prev = 0;
+        for(String s : bank)
         {
             int c = 0;
-            for(int j = 0; j<bank[i].length(); j++)
+            for(int i = 0; i<s.length(); i++)
             {
-                if(bank[i].charAt(j) == '1')
+                if(s.charAt(i) == '1')
+                {
                     c++;
+                }
             }
-            if(c!=0)
-                c1.add(c);
+            ans += (prev*c);
+            if(c > 0)
+                prev = c;
         }
-        if(c1.size()<=1)
-            return 0;
-        int lasers = 0;
-        for(int i = 0; i<c1.size()-1; i++)
-        {
-            lasers += (c1.get(i) * c1.get(i+1));
-        }
-        return lasers;
+        return ans;
     }
 }
