244c04aca3d22b63ccaa981162b345961fe3af85
Ayush Vaish <72246792+ayushvaish2511@users.noreply.github.com>
2024-01-12 19:53:31 +0530

    Time: 3 ms (88.24%), Space: 41.7 MB (34.03%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,16 +1,23 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        int ci = 0, cj = 0, i = 0, j = s.length()-1;
-        String vow = "AEIOUaeiou";
-        while(i<j)
+        String vow = "aeiouAEIOU";
+        int c1 = 0, c2 = 0;
+        int n = s.length();
+        for(int i = 0; i<n; i++)
         {
-            if(vow.indexOf(s.charAt(i)) != -1)
-                ci++;
-            if(vow.indexOf(s.charAt(j)) != -1)
-                cj++;
-            i++;
-            j--;
+            char ch = s.charAt(i);
+            if(vow.indexOf(ch) != -1)
+            {
+                if(i < n/2)
+                {
+                    c1++;
+                }
+                else 
+                {
+                    c2++;
+                }
+            }
         }
-        return ci==cj;
+        return c1 == c2;
     }
 }
